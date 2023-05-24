972ea80f3e28e440e2c48c97ac8a6479fc42e637
giangambin@gmail.com <giangambin@gmail.com>
2023-05-24 15:11:21 -0500

    .

--- lenguaje.js
+++ lenguaje.js
@@ -71,6 +71,279 @@
     recoverable: (boolean: TRUE when the parser has a error recovery rule available for this particular error)
   }
 */
+
+/*
+function eval(astNode) {
+      
+      var v;
+      switch(astNode.type) {
+          case 'function': 
+              // Function only has a left branch
+              v = eval(astNode.left); 
+              break;
+          case 'Statement': 
+              // Only need to eval the right hand side since thats your return statement
+              // The left hand side is either a no op, or the line before that does not matter
+              eval(astNode.left); 
+              v = eval(astNode.right); 
+              break; 
+          case 'array':
+              // Handle the right hand side of an array declaration
+              // Set the values to real values
+              var vec = [];
+              
+              var members = astNode.value;
+              for(var i=0;i<members.length;i++) {
+                  if(!members[i].name) {
+                      vec.push(members[i].value);
+                  } else {
+                      var identifierValue = executionstack.top()[members[i].name];
+                      if(!members[i].name in executionstack.top()) {
+                          throw "NameError: name '"+members[i].name+"' is not defined in list declaration\n";
+                      }
+                      vec.push(identifierValue);
+                  }
+              }
+              v = vec;
+              break;
+          case 'arrayindex':
+              // Handle rhs of a array index value retrieval
+              var identifierValue = executionstack.top()[astNode.name];
+              if(!astNode.name in executionstack.top()) {
+                  throw "NameError: name '"+astNode.name+"' is not defined\n";
+              }
+              v = identifierValue[parseInt(eval(astNode.index))]
+              break;
+          case 'len':
+              // Handle len()
+              var identifierValue = executionstack.top()[astNode.name];
+              if(!astNode.name in executionstack.top()) {
+                  throw "NameError: name '"+astNode.name+"' is not defined in list declaration\n";
+              }
+              
+              if(!Array.isArray(identifierValue)) {
+                  throw "TypeError: object of type '"+(typeof identifierValue)+"' has no len()";
+              }
+              v = identifierValue.length;
+              break; 
+          case 'method':
+              // Handle list.append(expr) and list.pop(expr)
+              var identifierValue = executionstack.top()[astNode.name];
+              if(!astNode.name in executionstack.top()) {
+                  throw "NameError: name '"+astNode.name+"' is not defined in list declaration\n";
+              }
+              if(!Array.isArray(identifierValue)) {
+                  throw "AttributeError: '"+(typeof identifierValue)+"' object has no attribute '"+astNode.method+"'";
+              }
+  
+              if(astNode.method == "append") {
+                  identifierValue.push(eval(astNode.argument));
+              } else if(astNode.method == "pop") {
+                  identifierValue.pop();				
+              } else {
+                  throw "AttributeError: '"+astNode.name+"' has no method '"+astNode.method+"'";
+              }
+              
+              break;	
+          case 'FunctionCall':
+              // Get function node and evaluate it
+              funcName = astNode.name;
+               functionNode = functions[funcName];
+              if(!funcName in functions) {
+                  throw "NameError: function named '"+funcName+"' is not defined";
+              }
+              
+              // Match given parameters to function signature in number only (no typing for piethon)
+              functionparams = functionNode.parameters;
+              callparams = astNode.parameters;
+              if(functionparams.length != callparams.length) {
+                  throw "TypeError: "+funcName+"() takes exactly "+functionparams.length+" arguments ("+callparams.length+" given)";
+              }
+              
+              // New stack with given params included to match signature
+              var newstackframe = {};
+              for(var i = 0;i<functionparams.length;i++) {
+                  // If an identifier verify it
+                  var callpari = callparams[i];
+                  var funcpari = functionparams[i];
+                  if(callpari.name) {
+                      var identifierValue = executionstack.top()[callpari.name];
+                      if(!callpari.name in executionstack.top()) {
+                          throw "NameError: name '"+astNode.name+"' is not defined\n";
+                      }
+                      newstackframe[funcpari.name] = identifierValue
+                  } else {
+                      // Otherwise just set to value
+                      newstackframe[funcpari.name] = callpari.value;
+                  }
+              }
+          
+              // Push new stack frame
+              executionstack.push(newstackframe);
+              // Call function
+              v = eval(functionNode);
+              
+              // Pop, back to old stack frame
+              executionstack.pop();
+              break;
+          case 'seraquesi': 
+              // If 
+              if(eval(astNode.left)) {
+                  v = eval(astNode.right); 	
+              }
+              break;
+          case 'ifelse': 
+              // If-else
+              if(eval(astNode.left)) {
+                  v = eval(astNode.middle); 	
+              } else {
+                  v = eval(astNode.right); 	
+              }
+              break;
+          case 'siempreycuando': 
+              // while
+              while(eval(astNode.left)) {
+                  v = eval(astNode.right); 	
+              }
+              break;
+          case 'dowhile':
+              // do-while
+              do {
+                  v = eval(astNode.left);
+              } while (eval(astNode.right));
+              break;
+          case 'hastaque':
+              // for loop
+              eval(astNode.left)
+              v = astNode.left.right.value;
+              let p = eval(astNode.right)
+              while(p){
+                  eval(astNode.body)
+                  eval(astNode.incremento)
+                  p = eval(astNode.right)
+              }		
+              break;
+          case 'IDENTIFICADOR': 
+              // Look up value in table
+              var identifierValue = executionstack.top()[astNode.name];
+              if(!astNode.name in executionstack.top()) {
+                  throw "NameError: name '"+astNode.name+"' is not defined\n";
+              }
+              v = identifierValue;
+              break;
+          case '=':
+              // Set value of identifier in table
+              if(astNode.left.type == 'arrayindex') {
+                  var vec2 = executionstack.top()[astNode.left.name];
+                  vec2[parseInt(eval(astNode.left.index))] = eval(astNode.right);
+              } else {
+                  executionstack.top()[astNode.left.name] = eval(astNode.right);	
+              }
+              break;
+          case '>':
+              if(eval(astNode.left) >  eval(astNode.right)) {
+                  v = true;
+              } else {
+                  v = false;
+              }
+              break;
+          case '>=':
+              if(eval(astNode.left) >=  eval(astNode.right)) {
+                  v = true;
+              } else {
+                  v = false;
+              }
+              break;
+          case '<':
+              if(eval(astNode.left) <  eval(astNode.right)) {
+                  v = true;
+              } else {
+                  v = false;
+              }
+              break;
+          case '<=':
+              if(eval(astNode.left) <=  eval(astNode.right)) {
+                  v = true;
+              } else {
+                  v = false;
+              }
+              break;
+  
+          case '==':
+              if(eval(astNode.left) == eval(astNode.right)) {
+                  v = true;
+              } else {
+                  v = false;
+              }
+              break;	
+          case '!=':
+              if(eval(astNode.left) != eval(astNode.right)) {
+                  v = true;
+              } else {
+                  v = false;
+              }
+              break;			
+          case 'no-op': 
+              // Do nothing!
+          break;
+          case 'dimelo':	
+              v = eval(astNode.left);
+              var strPrint;
+              if(Array.isArray(v)) {
+                  strPrint = '['+v.toString()+']';
+              } else {
+                  strPrint = v;
+              }
+              // Print 
+              console.log(strPrint);
+              break;
+          case 'llegapue': v = eval(astNode.left); break; 
+          case 'NUMBER': v = astNode.value; break;
+          case 'STRING': v = astNode.value.replace(/\"/g,''); break;
+          case '+': 
+              left = eval(astNode.left);
+              right = eval(astNode.right);
+              v = (left + right); 	
+              break;
+          case '++': 
+              left = eval(astNode.left);
+              console.log(left)	
+              break;
+          case '-': 
+              left = eval(astNode.left);
+              right = eval(astNode.right);
+              v = left - right; 	
+              break;
+          case '*': 
+              left = eval(astNode.left);
+              right = eval(astNode.right);
+              v = (left * right); 	
+              break;
+          case '/': 
+              left = eval(astNode.left);
+              right = eval(astNode.right);
+              v = (left / right); 	
+              break;		
+          case '%': 
+              left = eval(astNode.left);
+              right = eval(astNode.right);
+              v = (left % right); 	
+              break;		
+          case '**': 
+              left = eval(astNode.left);
+              right = eval(astNode.right);
+              v = Math.pow(left, right); 	
+              break;		
+          
+          case 'UMINUS': v = -1 * eval(astNode.left); break;
+          default: throw "internal error: bad node '"+astNode.type+"'";
+      }
+      return v;
+  }
+
+*/
+
+
 var lenguaje = (function(){
 var o=function(k,v,o,l){for(o=o||{},l=k.length;l--;o[k[l]]=v);return o},$V0=[2,10],$V1=[10,28,29,44],$V2=[2,26],$V3=[1,10],$V4=[1,6],$V5=[1,7],$V6=[1,8],$V7=[1,9],$V8=[1,13],$V9=[1,14],$Va=[1,15],$Vb=[1,10,13,17,18,21,22,23,28,29,44],$Vc=[1,28],$Vd=[1,31],$Ve=[1,32],$Vf=[1,33],$Vg=[10,13,17,18,21,22,23,28,29,44],$Vh=[2,6],$Vi=[7,10,15,28,29,44],$Vj=[1,48],$Vk=[1,49],$Vl=[1,50],$Vm=[1,51],$Vn=[1,52],$Vo=[1,53],$Vp=[1,54],$Vq=[1,55],$Vr=[1,56],$Vs=[1,57],$Vt=[1,58],$Vu=[1,59],$Vv=[7,10,15,26,28,29,30,31,32,33,34,35,36,37,38,39,40,41,44],$Vw=[2,41],$Vx=[1,61],$Vy=[15,26],$Vz=[7,10,15,26,28,29,30,31,44],$VA=[7,10,15,26,28,29,30,31,32,34,35,44];
 var parser = {trace: function trace () { },
